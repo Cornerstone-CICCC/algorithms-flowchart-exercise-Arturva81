37221bd2dde94a97efb3bd03cb02f7ca9f5b6606
Arturo Vazquez <arturva.81@gmail.com>
2025-09-17 17:57:05 -0700

    Changed answer to exercise 4

--- js/answer4.js
+++ js/answer4.js
@@ -1,8 +1,24 @@
 function isTrianglePossible(a, b, c){ 
-    if (a + b > c && b + c > a && a + c > b)
+    if ((a + b > c) &&
+        (b + c > a) && 
+        (a + c > b))
         return "Triangle is possible!";
     else
         return "Triangle is not possible";
 }
 
-console.log(isTrianglePossible(3, 4, 5));
+console.log(isTrianglePossible(5, 10, 10));
+
+
+function checkTriangle(a, b, c){
+    if (a + b > c){
+        if (b + c > a){
+            if (a + c > b){
+                return "Triangle is possible!";
+            }
+            else return "Triangle is not possible";
+        }
+    }
+}
+
+console.log(checkTriangle(10, 7, 8));
